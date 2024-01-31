cf3669649f1b3550c26f3713258169be6c1de175
Geoffrey Martin <geoffm39@gmail.com>
2024-02-01 08:49:16 +1100

    refactor TurtleScreen handling

--- gui/game_screen.py
+++ gui/game_screen.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from turtle import TurtleScreen
 
 from paddle import Paddle
 from ball import Ball
@@ -12,13 +13,14 @@ class GameScreen(Canvas):
         super().__init__(parent, **kwargs)
         self.configure(background='black')
 
-        self.paddle = Paddle(self)
+        self.screen = TurtleScreen(self)
+        self.configure_screen()
 
-        self.screen = self.paddle.getscreen()
-        self.screen.tracer(0)
-        self.screen.bgcolor('black')
-        self.screen.listen()
+        self.paddle = Paddle(self.screen)
 
+        self.apply_mouse_controls()
+
+    def apply_mouse_controls(self):
         self.bind('<Motion>', self.track_player_movement)
         self.bind('<Enter>', self.hide_mouse_cursor)
         self.bind('<Leave>', self.show_mouse_cursor)
@@ -33,3 +35,8 @@ class GameScreen(Canvas):
 
     def show_mouse_cursor(self, event):
         self.config(cursor='')
+
+    def configure_screen(self):
+        self.screen.tracer(0)
+        self.screen.bgcolor('black')
+        self.screen.listen()
