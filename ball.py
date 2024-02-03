da4356a3e75ea0130579f0983626329e8f045fd4
Geoffrey Martin <geoffm39@gmail.com>
2024-02-03 20:44:33 +1100

    add ball reflection calculations

--- ball.py
+++ ball.py
@@ -1,6 +1,6 @@
 from turtle import RawTurtle
 
-from constants import BALL_START_POSITION, VERTICAL, HORIZONTAL, NORTH, SOUTH, EAST, WEST
+from constants import BALL_START_POSITION, VERTICAL, HORIZONTAL, NORTH, SOUTH, EAST, WEST, COMPLETE_ANGLE
 
 
 class Ball(RawTurtle):
@@ -20,24 +20,37 @@ class Ball(RawTurtle):
         self.forward(1)
 
     def bounce(self, surface):
-        if self.is_moving_vertically():
-            pass
-        elif self.is_moving_west():
-            pass
-        elif self.is_moving_east():
-            pass
-
-    def is_moving_vertically(self):
         direction = self.heading()
+        if self.is_moving_vertically(direction):
+            self.calculate_vertical_movement(direction, surface)
+        elif self.is_moving_north_east(direction):
+            self.calculate_north_east_movement(direction, surface)
+        elif self.is_moving_north_west(direction):
+            self.calculate_north_west_movement(direction, surface)
+        elif self.is_moving_south_west(direction):
+            self.calculate_south_west_movement(direction, surface)
+        elif self.is_moving_south_east(direction):
+            self.calculate_south_east_movement(direction, surface)
+
+    @staticmethod
+    def is_moving_vertically(direction):
         return direction == NORTH or direction == SOUTH
 
-    def is_moving_east(self):
-        direction = self.heading()
-        return direction < NORTH or direction > SOUTH
+    @staticmethod
+    def is_moving_north_east(direction):
+        return EAST < direction < NORTH
 
-    def is_moving_west(self):
-        direction = self.heading()
-        return direction > NORTH or direction < SOUTH
+    @staticmethod
+    def is_moving_north_west(direction):
+        return NORTH < direction < WEST
+
+    @staticmethod
+    def is_moving_south_west(direction):
+        return WEST < direction < SOUTH
+
+    @staticmethod
+    def is_moving_south_east(direction):
+        return SOUTH < direction
 
     @staticmethod
     def is_vertical_surface(surface):
@@ -47,7 +60,53 @@ class Ball(RawTurtle):
     def is_horizontal_surface(surface):
         return surface == HORIZONTAL
 
-    def calculate_vertical_bounce(self, surface):
+    def calculate_vertical_movement(self, direction, surface):
         if self.is_vertical_surface(surface):
             return
-            # todo calculation here
+        if self.heading() == NORTH:
+            self.set_ball_direction(SOUTH)
+        else:
+            self.set_ball_direction(NORTH)
+
+    def calculate_north_east_movement(self, direction, surface):
+        if self.is_horizontal_surface(surface):
+            incidence_angle = direction
+            reflection_angle = COMPLETE_ANGLE - incidence_angle
+            self.set_ball_direction(reflection_angle)
+        else:
+            incidence_angle = NORTH - direction
+            reflection_angle = NORTH + incidence_angle
+            self.set_ball_direction(reflection_angle)
+
+    def calculate_north_west_movement(self, direction, surface):
+        if self.is_horizontal_surface(surface):
+            incidence_angle = WEST - direction
+            reflection_angle = WEST + incidence_angle
+            self.set_ball_direction(reflection_angle)
+        else:
+            incidence_angle = direction - NORTH
+            reflection_angle = NORTH - incidence_angle
+            self.set_ball_direction(reflection_angle)
+
+    def calculate_south_west_movement(self, direction, surface):
+        if self.is_horizontal_surface(surface):
+            incidence_angle = direction - WEST
+            reflection_angle = WEST - incidence_angle
+            self.set_ball_direction(reflection_angle)
+        else:
+            incidence_angle = SOUTH - direction
+            reflection_angle = SOUTH + incidence_angle
+            self.set_ball_direction(reflection_angle)
+
+    def calculate_south_east_movement(self, direction, surface):
+        if self.is_horizontal_surface(surface):
+            incidence_angle = COMPLETE_ANGLE - direction
+            reflection_angle = incidence_angle
+            self.set_ball_direction(reflection_angle)
+        else:
+            incidence_angle = direction - SOUTH
+            reflection_angle = SOUTH - incidence_angle
+            self.set_ball_direction(reflection_angle)
+
+    def set_ball_direction(self, direction):
+        self.setheading(direction)
