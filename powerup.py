6ffef5a3d7c37ff0f6c905d67ee257158e458cec
Geoffrey Martin <geoffm39@gmail.com>
2024-02-19 19:56:53 +1100

    add powerup and lives images

--- powerup.py
+++ powerup.py
@@ -1,5 +1,9 @@
 from turtle import RawTurtle
 
+from constants import (
+   MULTIBALL, FIREBALL, SLOW_BALL, FAST_BALL, LASERS, SMALL_PADDLE, BIG_PADDLE, EXTRA_LIFE
+)
+
 
 class Powerup(RawTurtle):
     def __init__(self, canvas, **kwargs):
