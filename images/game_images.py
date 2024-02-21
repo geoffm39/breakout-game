f380b76afda94824053922934528c10356c31fd5
Geoffrey Martin <geoffm39@gmail.com>
2024-02-21 23:39:07 +1100

    store resized paddle image as game_images attribute

--- images/game_images.py
+++ images/game_images.py
@@ -10,8 +10,9 @@ from constants import (
 
 class GameImages:
     def __init__(self):
-        self.images = {}
+        self.photo_images = {}
         self.paddle_image = None
+        self.resized_paddle = None
 
         self.load_images()
 
@@ -20,7 +21,7 @@ class GameImages:
             image_path = os.path.join(IMAGE_DIRECTORY, filename)
             with Image.open(image_path) as image:
                 photo_image = ImageTk.PhotoImage(image)
-            self.images[filename] = photo_image
+            self.photo_images[filename] = photo_image
 
     def get_brick_image(self, brick: Brick):
         brick_type = brick.get_type()
@@ -28,23 +29,23 @@ class GameImages:
             brick_type = NORMAL
         brick_color = brick.get_color()
         image_key = f'brick-{brick_color}-{brick_type}.png'
-        return self.images[image_key]
+        return self.photo_images[image_key]
 
     def get_background(self):
-        return self.images[BACKGROUND_FILENAME]
+        return self.photo_images[BACKGROUND_FILENAME]
 
     def get_paddle(self):
         image_path = os.path.join(IMAGE_DIRECTORY, PADDLE_FILENAME)
         with Image.open(image_path) as image:
             self.paddle_image = image.copy()
-        return self.images[PADDLE_FILENAME]
+        return self.photo_images[PADDLE_FILENAME]
 
     def get_resized_paddle(self, paddle_length):
         paddle_pixel_length = paddle_length * PADDLE_WIDTH
         resized_image = self.paddle_image.copy()
         resized_image = resized_image.resize((paddle_pixel_length, PADDLE_WIDTH))
-        self.images[PADDLE_FILENAME] = ImageTk.PhotoImage(resized_image)
-        return self.images[PADDLE_FILENAME]
+        self.resized_paddle = ImageTk.PhotoImage(resized_image)
+        return self.resized_paddle
 
     def get_powerup(self):
-        return self.images[POWERUP_FILENAME]
+        return self.photo_images[POWERUP_FILENAME]
